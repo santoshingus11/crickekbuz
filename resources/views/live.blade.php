7436693747c5d4c8df54aa388b55e602d75636c2
santoshingus11 <shahbaziqbal50@gmail.com>
2024-06-20 13:22:35 -0700

    first commit

---
+++ resources/views/live.blade.php
@@ -0,0 +1,23 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <title>Player</title>
+    <style>
+        body, html {
+            margin: 0;
+            padding: 0;
+            width: 100%;
+            height: 100%;
+            overflow: hidden;
+        }
+        iframe {
+            border: none;
+            width: 100%;
+            height: 100%;
+        }
+    </style>
+</head>
+<body>
+    <iframe src="/proxy?url=https://allinone-tataplay-web-one.vercel.app/player.html?channel=24" allowfullscreen></iframe>
+</body>
+</html>
